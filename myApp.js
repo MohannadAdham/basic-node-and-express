02bb8f704a1bae679939693c6cd7405d53dbc7f7
Mohannad Adham <mohannad.adm@gmail.com>
2024-02-24 00:23:50 +0000

    Add a app.get method to send an html file

--- myApp.js
+++ myApp.js
@@ -3,11 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
+// app.get('/', (req, res) => {
+//     res.send("Hello Express");
+// });
+
 app.get('/', (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-
 
 
 
